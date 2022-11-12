c25ae861db59d9038f12155f6d7cddceb85fdd1e
sirvin01 <91463820+sirvin01@users.noreply.github.com>
2022-11-12 21:59:08 +0300

    update

--- main.go
+++ main.go
@@ -4,5 +4,22 @@ import {
 	
 }
 func main(){
+	a, err := os.Open("abcd.txt")
+	if err != nil{
+		log.fatal(err)
+	}
+	defer a.Close()
+
+	b, err := ioutil.ReadAll(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	abc := string(b)
+	newQR := "abcd-qr.png"
+
+	err = qrcode.WriteFile(abc, qrcode.Medium, 512, newQR)
+
+	if err != 
 
 }
